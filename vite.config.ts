b6ca3078c16cf2b612e4b49021fb1fff3fa1c6b7
Ronit <r62544400@gmail.com>
2026-09-19 19:10:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Dentist-Clinic/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
